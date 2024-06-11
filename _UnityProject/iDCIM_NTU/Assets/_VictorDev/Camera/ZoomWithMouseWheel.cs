9ed1b2ddd748ad0695a3cc38852a997241c6688b
VictorZhanUnity <73773024+VictorZhanUnity@users.noreply.github.com>
2024-06-11 11:25:19 +0800

    更改背景

--- _UnityProject/iDCIM_NTU/Assets/_VictorDev/Camera/ZoomWithMouseWheel.cs
+++ _UnityProject/iDCIM_NTU/Assets/_VictorDev/Camera/ZoomWithMouseWheel.cs
@@ -10,7 +10,7 @@ namespace VictorDev.CameraHandler
     [RequireComponent(typeof(CinemachineFreeLook))]
     public class ZoomWithMouseWheel : MonoBehaviour
     {
-        [SerializeField] private float minRange = 50f, maxRange = 100f;
+        [SerializeField] private float minDistance = 50f, maxDistance = 100f;
         [SerializeField] private float minPosY = 0f, maxPosY = 100f;
         [SerializeField] private float zoomSpeed = 30f;
         [SerializeField] private float moveSpeed = 30f;
@@ -26,6 +26,49 @@ namespace VictorDev.CameraHandler
         {
             CameraDistanceControll();
             CameraPosYControll();
+        }
+
+
+        /// <summary>
+        /// 控制攝影機遠近
+        /// </summary>
+        private void CameraDistanceControll()
+        {
+            scrollWheelValue = Input.GetAxis("Mouse ScrollWheel") * -1;
+            float maxHeight = freeLookCamera.m_Orbits[0].m_Height;
+            float minHeight = freeLookCamera.m_Orbits[2].m_Height;
+
+            for (int i = 0; i < 3; i++)
+            {
+#if UNITY_EDITOR || UNITY_WEBGL
+                freeLookCamera.m_Orbits[i].m_Radius += scrollWheelValue * zoomSpeed;
+#elif PLATFORM_ANDROID
+                    freeLookCamera.m_Orbits[i].m_Radius += MobileInputHandler.ZoomByFingerScale();
+#endif
+                freeLookCamera.m_Orbits[i].m_Radius = Mathf.Clamp(freeLookCamera.m_Orbits[i].m_Radius, minDistance, maxDistance);
+            }
+
+            Debug.Log($"{maxHeight} / {minHeight}");
+
+            Vector3 pos = freeLookCamera.transform.position;
+            pos.y = Mathf.Clamp(pos.y + scrollWheelValue, minHeight, maxHeight);
+            freeLookCamera.transform.position = pos;
+        }
+
+        private void CameraDistanceControll1()
+        {
+            // 获取鼠标滚轮输入
+            float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+            if (scrollInput != 0)
+            {
+                // 根据输入计算新的距离
+                float currentDistance = Vector3.Distance(freeLookCamera.transform.position, freeLookCamera.Follow.position);
+                float targetDistance = Mathf.Clamp(currentDistance - scrollInput * zoomSpeed, minDistance, maxDistance);
+
+                // 设置新的Follow位置
+                Vector3 direction = (freeLookCamera.transform.position - freeLookCamera.Follow.position).normalized;
+                freeLookCamera.Follow.position = freeLookCamera.Follow.position + direction * targetDistance;
+            }
         }
 
         private void CameraPosYControll()
@@ -48,23 +91,6 @@ namespace VictorDev.CameraHandler
                 movePosVector.y = Mathf.Clamp(lookAtTarget.position.y - moveSpeed * Time.deltaTime, minPosY, maxPosY);
                 movePosVector.z = lookAtTarget.position.z;
                 lookAtTarget.position = movePosVector;
-            }
-        }
-
-        /// <summary>
-        /// 控制攝影機遠近
-        /// </summary>
-        private void CameraDistanceControll()
-        {
-            scrollWheelValue = Input.GetAxis("Mouse ScrollWheel") * -1;
-            for (int i = 0; i < 3; i++)
-            {
-#if UNITY_EDITOR || UNITY_WEBGL
-                freeLookCamera.m_Orbits[i].m_Radius += scrollWheelValue * zoomSpeed;
-#elif PLATFORM_ANDROID
-                    freeLookCamera.m_Orbits[i].m_Radius += MobileInputHandler.ZoomByFingerScale();
-#endif
-                freeLookCamera.m_Orbits[i].m_Radius = Mathf.Clamp(freeLookCamera.m_Orbits[i].m_Radius, minRange, maxRange);
             }
         }
 
